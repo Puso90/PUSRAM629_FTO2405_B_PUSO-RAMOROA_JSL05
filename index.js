63b3a75faff3e0c1cea1147822ad5480cfd17ff7
Puso90 <juwishbrands@gmail.com>
2024-07-25 23:25:43 +0200

    added songs to playlist

--- index.js
+++ index.js
@@ -12,6 +12,11 @@ const songs = [
     { title: "I'm Not in Love", artist: "10cc", genre: "Pop" },
     { title: "Fooled Around and Fell in Love", artist: "Elvin Bishop", genre: "Rock" },
     // Feel free to add even more songs
+    { title: "Billie Jean", artist: "Michael Jackson", genre: "Pop"},
+    { title: "U Got It Bad", artist: "Usher", genre: "R&B"},
+    { title: "Pantsula 4 Life", artist: "Booga Luv", genre: "Kwaito"},
+    { title: "Zonke", artist: "Booga Luv", genre: "Kwaito"},
+    { title: "Akekh'Ugogo", artist: "Mzekezeke", genre: "Kwaito"}
 ]; 
 
 
